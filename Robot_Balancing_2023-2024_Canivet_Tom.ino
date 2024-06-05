fae6482b27ba9e66402ef3f94b2efdb810f5933e
Tom1050 <tom.canivet@inraci.be>
2024-06-06 00:01:44 +0200

    Update Robot_Balancing_2023-2024_Canivet_Tom.ino
    dérivé1.0

--- Robot_Balancing_2023-2024_Canivet_Tom.ino
+++ Robot_Balancing_2023-2024_Canivet_Tom.ino
@@ -6,7 +6,8 @@
 
 #define _10ms 10
 
-float ACCEL_YANGLE;         //variable de l'axe X et Y de l'accéléromètre du MPU6050
+float ACCEL_YANGLE1;         //variable de l'axe X et Y de l'accéléromètre du MPU6050
+float ACCEL_YANGLE2;
 float x_out, y_out, z_out;  //variable de la sortie de l'axe X et Y de l'accéléromètre du MPU6050
 
 MPU6050 mpu;     //initialisation du MPU6050
@@ -24,25 +25,28 @@ int erreur1 = 0;
 int erreur2 = 0;
 int consigne = 0;
 
-unsigned long previousMillis = 0;  //variable de la précédente millis
+int derive = 0;
+
+  unsigned long previousMillis = 0;  //variable de la précédente millis
 // Complementary filter variables
 float angle = 0;     // Filtered angle / variable de la valeur de l'angle
 float gyroRate = 0;  // Gyro rate / initalisation de la vitesse du gyroscope
 float alpha = 0.98;  // Filter coefficient / variable du coéficient du filtre
 
 
-void Get_Accel_Angles() {
-  int16_t ax, ay, az, gx, gy, gz;  // variable de l'accéléromètre et du gyscope avec l'axe X, Y et Z
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+void Get_Accel_Angles1() {
 
-  // Accelerometer data
+  int16_t ax, ay, az, gx, gy, gz;  // variable de l'accéléromètre et du gyscope avec l'axe X, Y et Z
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);  //erreur ici
+
+  //Accelerometer data
   x_out = ax / 16384.0;
   y_out = ay / 16384.0;
   z_out = az / 16384.0;
 
-  ACCEL_YANGLE = 57.295 * atan(-x_out / sqrt(pow(z_out, 2) + pow(y_out, 2)));
+  ACCEL_YANGLE1 = 57.295 * atan(-x_out / sqrt(pow(z_out, 2) + pow(y_out, 2)));
 
-  // Gyroscope data
+  //Gyroscope data
   gyroRate = gy / 65.5;  // Convert gyro rate to degrees per second
 
   // Complementary filter
@@ -53,6 +57,29 @@ void Get_Accel_Angles() {
   angle = alpha * (angle + gyroRate * dt) + (1 - alpha) * ACCEL_YANGLE;
 }
 
+
+void Get_Accel_Angles2() {
+
+  int16_t ax, ay, az, gx, gy, gz;  // variable de l'accéléromètre et du gyscope avec l'axe X, Y et Z
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);  //erreur ici
+
+  //Accelerometer data
+  x_out = ax / 16384.0;
+  y_out = ay / 16384.0;
+  z_out = az / 16384.0;
+
+  ACCEL_YANGLE2 = 57.295 * atan(-x_out / sqrt(pow(z_out, 2) + pow(y_out, 2)));
+
+  //Gyroscope data
+  gyroRate = gy / 65.5;  // Convert gyro rate to degrees per second
+
+  // Complementary filter
+  unsigned long currentMillis = millis();
+  float dt = (currentMillis - previousMillis) / 1000.0;
+  previousMillis = currentMillis;
+
+  angle = alpha * (angle + gyroRate * dt) + (1 - alpha) * ACCEL_YANGLE;
+}
 
 int limite(int val, int lim_haute, int lim_basse) {
   if (val > lim_haute)
@@ -88,13 +115,14 @@ void setup() {
 }
 
 void loop() {
+
   if (millis() >= tempsMemoriser + _10ms)  // si millis est égale au temps Mémoriser plus 10millisecond
   {
 
     tempsMemoriser = millis();  // Mise à jour de t pour le prochain passage
+ 
 
-
-    Get_Accel_Angles();
+    Get_Accel_Angles1();
 
     if (vd_1s++ >= 100)  //boucle de 1 seconde
     {
@@ -103,22 +131,35 @@ void loop() {
       digitalWrite(LED, Etat_LED);
     }
 
+   /* if (vd_10ms++ >= 1)  //boucle toute les 40millisecond
+    {
+      vd_10ms = 0;
+      tempChute = (erreur1 * );
+    }*/
+
     if (vd_20ms++ >= 2)  //boucle toute les 20millisecond pour commander les servomoteurs
     {
-      int tempCalcul=0;
+
+      int tempCalcul = 0;
       vd_20ms = 0;
-      erreur1 = int(90 - ACCEL_YANGLE);
-      erreur2 = int(90 + ACCEL_YANGLE);
+      Get_Accel_Angle2();
+      erreur1 = int(86.6 - ACCEL_YANGLE);
+      erreur2 = int(98.5 + ACCEL_YANGLE);
 
-    tempCalcul=(erreur1-90);
+      derive = ACCEL_YANGLE2 * 3.5 - ACCEL_YANGLE1;
 
-     erreur1= limite(erreur1+tempCalcul,180, -180);
-     erreur2= limite(erreur2-tempCalcul, 180, -180);
-    
-   
-     myservo2.write(erreur2);
-     myservo1.write(erreur1);
+      tempCalcul = int(erreur1 - 90) * derive;
+
+      erreur1 = int(limite(erreur1 + tempCalcul, 180, 0));
+      erreur2 = int(limite(erreur2 - tempCalcul, 180, 0));
+      
+
+
+
+      myservo2.write(erreur2);
+      myservo1.write(erreur1);
     }
+
     if (vd_40ms++ >= 4)  //boucle toute les 40millisecond
     {
       vd_40ms = 0;
@@ -127,10 +168,7 @@ void loop() {
     {
       vd_100ms = 0;
 
-      Serial.printf("\n YANGLE : %07.3f err1:%d err2:%d", ACCEL_YANGLE,erreur1,erreur2);
+      Serial.printf("\n YANGLE : %07.3f err1:%d err2:%d", ACCEL_YANGLE, erreur1, erreur2);
     }
   }
-
-
-  //Serial.printf("\nAngle Y :%07.3f", Input);
 }
