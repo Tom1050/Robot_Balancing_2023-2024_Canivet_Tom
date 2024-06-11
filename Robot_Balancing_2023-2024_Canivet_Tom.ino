e1ce9cd6f60836a6747140d03fea5ad8b86e2cb9
Tom1050 <tom.canivet@inraci.be>
2024-06-11 08:32:51 +0200

    Update Robot_Balancing_2023-2024_Canivet_Tom.ino
    integrale2.0

--- Robot_Balancing_2023-2024_Canivet_Tom.ino
+++ Robot_Balancing_2023-2024_Canivet_Tom.ino
@@ -3,11 +3,11 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include <ESP32Servo.h>
+#include <BluetoothSerial.h>
 
 #define _10ms 10
 
-float ACCEL_YANGLE1;  //variable de l'axe X et Y de l'accéléromètre du MPU6050
-float ACCEL_YANGLE2;
+float ACCEL_YANGLE;         //variable de l'axe X et Y de l'accéléromètre du MPU6050
 float x_out, y_out, z_out;  //variable de la sortie de l'axe X et Y de l'accéléromètre du MPU6050
 
 MPU6050 mpu;     //initialisation du MPU6050
@@ -18,48 +18,30 @@ double Setpoint, Input;
 
 int vd_1s = 0;
 int vd_20ms = 0;
-int vd_40ms = 0;
+int vd_250ms = 0;
+int vd_260ms = 0;
 int vd_100ms = 0;
 
+int aerreur = 0;
+int berreur = 0;
 int erreur1 = 0;
 int erreur2 = 0;
 int consigne = 0;
 
-int integral = 0;
 
 unsigned long previousMillis = 0;  //variable de la précédente millis
 // Complementary filter variables
-float angle1 = 0;
-float angle2 = 0;    // Filtered angle / variable de la valeur de l'angle
+float angle = 0;     // Filtered angle / variable de la valeur de l'angle
 float gyroRate = 0;  // Gyro rate / initalisation de la vitesse du gyroscope
 float alpha = 0.98;  // Filter coefficient / variable du coéficient du filtre
 
+// Bluetooth variables
+const char *pin = "1234";             // code pour le bluetooth du micrcontrôleur
+String myName = "robot_balance";      //
+String slaveName = "ESP32-BT-Slave";  // le nom qui apparaît dans le bluetooth
+BluetoothSerial SerialBT;             //initialisation du bluetooth du microcontrôleur
 
-void Get_Accel_Angles1() {
-
-  int16_t ax, ay, az, gx, gy, gz;                // variable de l'accéléromètre et du gyscope avec l'axe X, Y et Z
-  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);  //erreur ici
-
-  //Accelerometer data
-  x_out = ax / 16384.0;
-  y_out = ay / 16384.0;
-  z_out = az / 16384.0;
-
-  ACCEL_YANGLE1 = 57.295 * atan(-x_out / sqrt(pow(z_out, 2) + pow(y_out, 2)));
-
-  //Gyroscope data
-  gyroRate = gy / 65.5;  // Convert gyro rate to degrees per second
-
-  // Complementary filter
-  unsigned long currentMillis = millis();
-  float dt = (currentMillis - previousMillis) / 1000.0;
-  previousMillis = currentMillis;
-
-  angle1 = alpha * (angle1 + gyroRate * dt) + (1 - alpha) * ACCEL_YANGLE1;
-}
-
-
-void Get_Accel_Angles2() {
+void Get_Accel_Angles() {
 
   int16_t ax, ay, az, gx, gy, gz;                // variable de l'accéléromètre et du gyscope avec l'axe X, Y et Z
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);  //erreur ici
@@ -69,7 +51,7 @@ void Get_Accel_Angles2() {
   y_out = ay / 16384.0;
   z_out = az / 16384.0;
 
-  ACCEL_YANGLE2 = 57.295 * atan(-x_out / sqrt(pow(z_out, 2) + pow(y_out, 2)));
+  ACCEL_YANGLE = 57.295 * atan(-x_out / sqrt(pow(z_out, 2) + pow(y_out, 2)));
 
   //Gyroscope data
   gyroRate = gy / 65.5;  // Convert gyro rate to degrees per second
@@ -79,8 +61,9 @@ void Get_Accel_Angles2() {
   float dt = (currentMillis - previousMillis) / 1000.0;
   previousMillis = currentMillis;
 
-  angle2 = alpha * (angle2 + gyroRate * dt) + (1 - alpha) * ACCEL_YANGLE2;
+  angle = alpha * (angle + gyroRate * dt) + (1 - alpha) * ACCEL_YANGLE;
 }
+
 
 int limite(int val, int lim_haute, int lim_basse) {
   if (val > lim_haute)
@@ -101,6 +84,7 @@ void setup() {
   tempsMemoriser = millis();  // Initialisation du temps t
   Wire.begin();
   Serial.begin(115200);
+  SerialBT.begin(slaveName);
   myservo1.attach(21);
   myservo2.attach(19);
   delay(1);
@@ -123,7 +107,7 @@ void loop() {
     tempsMemoriser = millis();  // Mise à jour de t pour le prochain passage
 
 
-    Get_Accel_Angles1();
+    Get_Accel_Angles();
 
     if (vd_1s++ >= 100)  //boucle de 1 seconde
     {
@@ -132,45 +116,49 @@ void loop() {
       digitalWrite(LED, Etat_LED);
     }
 
-  /*  if (vd_10ms++ >= 1)  //boucle toute les 40millisecond
+    /*  if (vd_10ms++ >= 1)  //boucle toute les 40millisecond
     {
       vd_10ms = 0;
       tempChute = (erreur1*);
     }*/
 
-    if (vd_20ms++ >= 2)  //boucle toute les 20millisecond pour commander les servomoteurs
+    if (vd_20ms++ >= 1)  //boucle toute les 20millisecond pour commander les servomoteurs
     {
 
-      int tempCalcul1 = 0;
-      int tempCalcul2 = 0;
+      int tempCalcul = 0;
+
       vd_20ms = 0;
+      Get_Accel_Angles();
+      erreur1 = int(87 - ACCEL_YANGLE);
+      erreur2 = int(91 + ACCEL_YANGLE);
 
-      erreur1 = int(89 - ACCEL_YANGLE1);
-      erreur2 = int(96.5 + ACCEL_YANGLE2);
+      tempCalcul = int(((erreur1 - 90) * 10) - aerreur);
 
-      integral = int(ACCEL_YANGLE2 * 3.3 - ACCEL_YANGLE1);
-
-      tempCalcul1 = int(erreur1 + 90) * integral;
-      tempCalcul2 = int(erreur1 + 90) * integral;
-
-      erreur1 = int(limite(erreur1 - tempCalcul2, 180, 0));
-      erreur2 = int(limite(erreur2 + tempCalcul1, 180, 0));
+      erreur1 = int(limite(erreur1 + tempCalcul, 180, 0));
+      erreur2 = int(limite(erreur2 - tempCalcul, 180, 0));
 
       myservo2.write(erreur2);
       myservo1.write(erreur1);
-
-      Get_Accel_Angles2();
+      berreur = ACCEL_YANGLE;
     }
 
-    if (vd_40ms++ >= 4)  //boucle toute les 40millisecond
+    if (vd_250ms++ >= 25)  //boucle toute les 500millisecond
     {
-      vd_40ms = 0;
+      vd_250ms = 0;
+      aerreur = berreur;
+      aerreur = aerreur - erreur1;
+    }
+     if (vd_260ms++ >= 26)  //boucle toute les 500millisecond
+    {
+      vd_260ms = 0;
+      //aerreur = 0;
     }
     if (vd_100ms++ >= 10)  //boucle toute les 100millisecond
     {
       vd_100ms = 0;
 
-      Serial.printf("\n YANGLE1 : %07.3f YANGLE2 : %07.3f err1:%d err2:%d integral : %07.3f", ACCEL_YANGLE1, ACCEL_YANGLE2, erreur1, erreur2, integral);
+      Serial.printf("\n YANGLE : %07.3f err1:%d err2:%d, aerreur:%d", ACCEL_YANGLE, erreur1, erreur2, aerreur);
+      SerialBT.printf("\n YANGLE : %07.3f err1:%d err2:%d", ACCEL_YANGLE, erreur1, erreur2);
     }
   }
 }
